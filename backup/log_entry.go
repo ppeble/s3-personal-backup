f7cd2f0e0ff1c509210e479844e4c504c12dc21c
Phil Trimble <ptrimble@sittercity.com>
2016-06-13 16:33:39 +0000

    Add LogEntry.String() unit test

--- backup/log_entry.go
+++ backup/log_entry.go
@@ -8,7 +8,6 @@ type LogEntry struct {
 	Message, File, Level string
 }
 
-//TODO This should probably have a matching test for the sake of completeness
 func (l LogEntry) String() string {
 	return fmt.Sprintf("file: '%s' - message: '%s'", l.File, l.Message)
 }
---
+++ backup/log_entry_test.go
@@ -0,0 +1,17 @@
+package backup
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_LogEntry_String(t *testing.T) {
+	entry := LogEntry{
+		Message: "Message",
+		File:    "File",
+		Level:   "Level",
+	}
+
+	assert.Equal(t, "file: 'File' - message: 'Message'", entry.String())
+}
